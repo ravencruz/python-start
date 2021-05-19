b02493064f3f0ccfaa1a685d6ea8b1681284e921
jsebasct <jsebasct@gmail.com>
2021-05-19 15:13:23 -0300

    decades by teacher

--- decades.py
+++ decades.py
@@ -1,7 +1,8 @@
 age = input('How old are you?\n')
 age = int(age)
 
-decades = int(age / 10)
+#decades = int(age / 10)
+decades = age // 10
 years = age % 10
 
 print('You are ' + str(decades) + ' decade(s) and ' + str(years) + ' year(s) old')
