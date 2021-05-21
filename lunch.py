bdfed25373aac585b6037ed29e81768bf01c4fd2
jsebasct <jsebasct@gmail.com>
2021-05-21 15:45:20 -0300

    feat: demo dictionaries

---
+++ lunch.py
@@ -0,0 +1,8 @@
+menus = {
+    'Breakfast': ['Egg Sandich', 'Bagel'],
+    'Lunch': ['BLT', 'PB&J'],
+    'Dinner': ['Soup', 'Salad'],
+}
+
+for name, menu in menus.items():
+    print(name, ':', menu)
